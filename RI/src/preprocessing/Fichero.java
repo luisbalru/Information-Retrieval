c7328b7619a46402501d945cd39e871a17280ad0
Luis Balderas Ruiz <balderasruizluis@gmail.com>
2018-10-03 15:59:18 +0200

    Close #6

---
+++ RI/src/preprocessing/Fichero.java
@@ -0,0 +1,149 @@
+/*
+ * Definición del nuevo T.D.A: Fichero
+ */
+
+/*
+ * @author Luis Balderas Ruiz
+ */
+
+package preprocessing;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import org.apache.tika.Tika;
+import org.apache.tika.detect.AutoDetectReader;
+import org.apache.tika.exception.TikaException;
+import org.apache.tika.metadata.Metadata;
+import org.apache.tika.parser.AutoDetectParser;
+import org.apache.tika.parser.ParseContext;
+import org.apache.tika.parser.Parser;
+import org.apache.tika.sax.BodyContentHandler;
+import org.apache.tika.sax.Link;
+import org.apache.tika.sax.LinkContentHandler;
+import org.xml.sax.SAXException;
+import org.apache.tika.langdetect.OptimaizeLangDetector;
+import org.apache.tika.language.detect.LanguageDetector;
+import org.apache.tika.language.detect.LanguageResult;
+import org.apache.tika.sax.TeeContentHandler;
+
+public class Fichero{
+  String name;
+  ArrayList<String> metadata;
+  String language;
+  String type;
+  String charset;
+  String content;
+  List<Link> links;
+  HashMap<String,Integer> word_count;
+
+  public Fichero(String path) throws IOException, SAXException, TikaException{
+    Tika tika = new Tika();
+    File f = new File(path);
+    name = f.getName();
+    type = tika.detect(f);
+    charset = setCharset(f,tika);
+    metadata = new ArrayList<String>();
+    links = new ArrayList<Link>();
+    word_count = new HashMap<String, Integer>();
+	getContent_Metadata(f);
+	cuentaPalabras();
+    language = identifyLanguage(content);
+  }
+
+  private String setCharset(File f, Tika tika) throws FileNotFoundException{
+	FileInputStream inputstream = new FileInputStream(f);
+	AutoDetectReader detector = null;
+	try {
+		detector = new AutoDetectReader(inputstream);
+	} catch (IOException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	} catch (TikaException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+    Charset charSet = detector.getCharset();
+    return charSet.toString();
+  }
+
+
+  private void getContent_Metadata(File f) throws IOException, SAXException, TikaException{
+	  // Elimina el límite de contenido
+	FileInputStream inputstream = new FileInputStream(f);
+	Parser parser = new AutoDetectParser();
+    BodyContentHandler handler = new BodyContentHandler(-1);
+    LinkContentHandler link_handler = new LinkContentHandler();
+    TeeContentHandler teeHandler = new TeeContentHandler(link_handler,handler);
+    Metadata met = new Metadata();
+    ParseContext context = new ParseContext();
+    parser.parse(inputstream, teeHandler, met, context);
+    inputstream.close();
+    content = handler.toString();
+    links = link_handler.getLinks();
+    String[] metadataNames = met.names();
+    String aux;
+    for(String name : metadataNames){
+      aux = name + ": " + met.get(name);
+      metadata.add(aux);
+    }
+  }
+  private String identifyLanguage(String text) throws IOException{
+	  LanguageDetector identifier  = new  OptimaizeLangDetector().loadModels();
+      LanguageResult idioma = identifier.detect(text);
+      return idioma.getLanguage();
+  }
+  
+  private void cuentaPalabras() {
+	  String[] split = content.split("\\s");
+	  for(String st : split) {
+		  if(word_count.containsKey(st)) {
+			  int num = word_count.get(st) + 1;
+			  word_count.put(st, num);
+		  }
+		  else
+			  word_count.put(st, 0);
+	  }
+  }
+  
+  public HashMap<String, Integer> getPalabras(){
+	  return word_count;
+  }
+
+  	
+  public String getName(){
+    return name;
+  }
+
+  public String getType(){
+    return type;
+  }
+
+  public String getLang(){
+    return language;
+  }
+
+  public String getCharset(){
+    return charset;
+  }
+
+  public String getContent(){
+    return content;
+  }
+
+  public ArrayList<String> getMetadata(){
+    return metadata;
+  }
+  
+  public List<Link> getLinks(){
+	  return links;
+  }
+  
+  
+}
---
+++ RI/src/preprocessing/Preprocesamiento.java
@@ -0,0 +1,94 @@
+/**
+ * Clase principal de la práctica 1, dedicada al preprocesamiento
+ */
+
+/**
+ * @author Luis Balderas Ruiz
+ */
+
+package preprocessing;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.collections4.iterators.ReverseListIterator;
+import org.apache.tika.exception.TikaException;
+import org.apache.tika.sax.Link;
+import org.xml.sax.SAXException;
+
+
+public class Preprocesamiento{
+	
+  public static void imprimeDatos(ArrayList<Fichero> ficheros) throws FileNotFoundException, UnsupportedEncodingException {
+	  PrintWriter writer = new PrintWriter("datoslibros.md", "UTF-8");
+	    
+	    for(int i=0; i<ficheros.size(); i++){
+	    	writer.println("| NOMBRE | TIPO | CHARSET | IDIOMA |");
+	    	writer.println("|--------|--------|---------|---------|");
+	    	writer.println(" | "+ ficheros.get(i).getName() +" | "+ ficheros.get(i).getType() +
+					" | "+ ficheros.get(i).getCharset() +" | " + ficheros.get(i).getLang()+" | " );
+	    }
+	    writer.close();
+  }
+  
+  public static void imprimeLinks(ArrayList<Fichero> ficheros) throws FileNotFoundException, UnsupportedEncodingException {
+	  for(int i=0; i<ficheros.size(); i++) {
+	  	PrintWriter writer = new PrintWriter("links_" + ficheros.get(i).getName()+".txt", "UTF-8");
+	  	List<Link> links = ficheros.get(i).getLinks();
+	  	for(Link enlace : links)
+	  		writer.println(enlace.toString());
+	  }	
+  }
+  
+  
+  private static List ordena(HashMap<String, Integer> m) {
+	  List lista = new LinkedList(m.entrySet());
+	  Collections.sort(lista, new Comparator() {
+		  	public int compare(Object o1, Object o2) {
+		  		return ((Comparable) (((Map.Entry) (o1)).getValue())).compareTo(((Map.Entry) (o2)).getValue());
+		  	}
+	  });
+	  
+	  return lista;
+  }
+  
+  public static void cuentaPalabras(ArrayList<Fichero> f) throws FileNotFoundException, UnsupportedEncodingException {
+	  List list;
+	  for(int i=0; i<f.size(); i++) {
+		  PrintWriter writer = new PrintWriter("word_count_" + f.get(i).getName() + ".txt", "UTF-8");
+		  list = ordena(f.get(i).getPalabras());
+		  Iterator it = new ReverseListIterator(list);
+		  while(it.hasNext()) {
+			  writer.println(it.next().toString());
+		  }
+		  writer.close();
+	  }
+  }
+
+  public static void main(String[] args) throws IOException, SAXException, TikaException{
+    FileExtraction extract = new FileExtraction();
+    ArrayList<String> paths = extract.getPaths(args[0]);
+    ArrayList<Fichero> ficheros = new ArrayList<Fichero>();
+    	
+    /*for(int i=1; i<paths.size(); i++){
+    	Fichero f = new Fichero(paths.get(0) + "/" + paths.get(i));
+    	ficheros.add(f);
+    }*/
+    
+    Fichero f = new Fichero(paths.get(0) + "/" + paths.get(9));
+	ficheros.add(f);
+    imprimeDatos(ficheros);
+    imprimeLinks(ficheros);
+    cuentaPalabras(ficheros);
+  }
+}
