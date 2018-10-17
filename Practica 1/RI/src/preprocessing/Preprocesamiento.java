8897d4e13c4451c61aa9330fdd0b64f2b4993350
Luis Balderas Ruiz <balderasruizluis@gmail.com>
2018-10-17 14:06:18 +0200

    Cambiando error

--- Practica 1/RI/src/preprocessing/Preprocesamiento.java
+++ Practica 1/RI/src/preprocessing/Preprocesamiento.java
@@ -151,14 +151,12 @@ public class Preprocesamiento{
 	    	    for(int i=0; i<ficheros.size();i++) {
 	    	    	CSV(ficheros.get(i));
 	    	    	String csv = ficheros.get(i).directorio+"/"+ficheros.get(i).getName()+"_datos.csv";
-		    	    String name = ficheros.get(i).directorio+"/"+ficheros.get(i).getName();
-	    	    	String csv = ficheros.get(0).directorio+"/"+ficheros.get(i).getName()+"_datos.csv";
-		    	    String name = ficheros.get(0).directorio+"/"+ficheros.get(i).getName();
-		    	    ProcessBuilder pb = new ProcessBuilder("python", "/home/luisbalru/plot.py", csv,name);
-		    	    Process p = pb.start();
-		    	    p.waitFor();
-		    	    System.out.println(p.exitValue());
-		    	    p.destroy();
+		    	String name = ficheros.get(i).directorio+"/"+ficheros.get(i).getName();
+		    	ProcessBuilder pb = new ProcessBuilder("python", "/home/luisbalru/plot.py", csv,name);
+		    	Process p = pb.start();
+		    	p.waitFor();
+		    	System.out.println(p.exitValue());
+		    	p.destroy();
 
 	    	    }
 	    	    keep = false;
