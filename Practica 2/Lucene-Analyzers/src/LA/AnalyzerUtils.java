8529f1ff9f83472c830f981d4f02776a1bd9174f
Luis Balderas Ruiz <balderasruizluis@gmail.com>
2018-10-20 02:12:48 +0200

    Clase con los analizadores. Añadido un último por idioma. Close #10

--- Practica 2/Lucene-Analyzers/src/LA/AnalyzerUtils.java
+++ Practica 2/Lucene-Analyzers/src/LA/AnalyzerUtils.java
@@ -10,8 +10,7 @@ import org.apache.lucene.analysis.es.SpanishAnalyzer;
 import org.apache.lucene.analysis.core.StopAnalyzer;
 import org.apache.lucene.analysis.core.SimpleAnalyzer;
 import org.apache.lucene.analysis.core.WhitespaceAnalyzer;
-
-
+import org.apache.lucene.analysis.en.EnglishAnalyzer;
 import org.apache.lucene.analysis.TokenStream;
 import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
 import org.apache.lucene.analysis.tokenattributes.OffsetAttribute;
@@ -27,5 +26,33 @@ import java.util.List;
 import org.apache.lucene.analysis.standard.StandardTokenizer;
 
 public class AnalyzerUtils {
+	String contenido;
+	public static final Analyzer [] analizadores = {
+            new WhitespaceAnalyzer(),
+            new SimpleAnalyzer(),
+            new StopAnalyzer(),
+            new StandardAnalyzer( ),
+            new UAX29URLEmailAnalyzer()
+            };
+	
+	public static List<String> tokenizeString(Analyzer analyzer, String string) {
+	    List<String> result = new ArrayList<String>();
+
+	    String cad;
+	    try {
+	      TokenStream stream  = analyzer.tokenStream(null, new StringReader(string));
+	      OffsetAttribute offsetAtt = stream.addAttribute(OffsetAttribute.class);
+	      CharTermAttribute cAtt= stream.addAttribute(CharTermAttribute.class);
+	      stream.reset();
+	      while (stream.incrementToken()) {
+	        result.add( cAtt.toString());
+	      }
+	      stream.end();
+	    } catch (IOException e) {
+	      // not thrown b/c we're using a string reader...
+	      throw new RuntimeException(e);
+	    }
+	    return result;
+	  }
 
 }
