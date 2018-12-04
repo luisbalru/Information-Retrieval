6bf5873189b01de4c8cec15e26e6341e26cfc4ad
Luis Balderas Ruiz <balderasruizluis@gmail.com>
2018-12-04 14:37:43 +0100

    Seguimos con #15

--- Practica 3/Searcher/src/Buscador.java
+++ Practica 3/Searcher/src/Buscador.java
@@ -7,13 +7,17 @@ import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.index.DirectoryReader;
 import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.index.Term;
 import org.apache.lucene.queryparser.classic.ParseException;
 import org.apache.lucene.queryparser.classic.QueryParser;
 import org.apache.lucene.search.BooleanClause;
 import org.apache.lucene.search.BooleanQuery;
 import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.search.Query;
+import org.apache.lucene.search.TermQuery;
 import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.Sort;
+import org.apache.lucene.search.SortField;
 import org.apache.lucene.search.TopDocs;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
@@ -27,9 +31,38 @@ public class Buscador {
 		answers_path = FileSystems.getDefault().getPath(ans_path);
 	}
 	
+	public ArrayList<Document> SearchAnswers(String idq) throws IOException{
+		ArrayList<Document> answers = new ArrayList<Document>();
+		Directory dir = FSDirectory.open(answers_path);
+		IndexReader ireader = DirectoryReader.open(dir);
+		IndexSearcher isearcher = new IndexSearcher(ireader);
+		
+		Query q1 = new TermQuery(new Term("ID-q",idq));
+		Query q2 = new TermQuery(new Term("aceptada","TRUE"));
+		
+		SortField sf = new SortField("punt-num",SortField.Type.FLOAT,true);
+		//sf.setMissingValue();
+		Sort orden = new Sort(sf);
+		
+		BooleanQuery.Builder cons = new BooleanQuery.Builder();
+		cons.add(q1,BooleanClause.Occur.MUST);
+		cons.add(q2,BooleanClause.Occur.MUST);
+		BooleanQuery q = cons.build();
+		TopDocs hits = isearcher.search(q, 5,orden);
+		for(int i=0; i<hits.scoreDocs.length; i++) {
+			Document hitDoc = isearcher.doc(hits.scoreDocs[i].doc);
+			answers.add(hitDoc);
+		}
+		ireader.close();
+		dir.close();
+		
+		return answers;
+	}
+	
 	public ArrayList<Document> SearchQuery(String q1, String q2, String f1, 
 			String f2, boolean must, boolean should, boolean not, int num_q) throws IOException, ParseException {
 		ArrayList<Document> queries = new ArrayList<Document>();	
+		ArrayList<Document> answers = new ArrayList<Document>();
 		Directory dir = FSDirectory.open(queries_path);
 		IndexReader ireader = DirectoryReader.open(dir);
 		IndexSearcher isearcher = new IndexSearcher(ireader);
@@ -49,15 +82,17 @@ public class Buscador {
 			Cons.add(query2,BooleanClause.Occur.MUST_NOT);
 		
 		BooleanQuery q = Cons.build();
-		System.out.println(q);
 		TopDocs hits = isearcher.search(q, num_q);
 		for(int i=0; i<hits.scoreDocs.length; i++) {
 			Document hitDoc = isearcher.doc(hits.scoreDocs[i].doc);
 			queries.add(hitDoc);
+			answers = SearchAnswers(hitDoc.getField("ID-q").stringValue());
 		}
 		ireader.close();
 		dir.close();
 		
 		return queries;
 	}
+	
+
 }
