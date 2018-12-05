12c3a134543cc14fe4000d5d43f7605690024548
Luis Balderas Ruiz <balderasruizluis@gmail.com>
2018-12-05 19:52:21 +0100

    #15 Panel para Salida

---
+++ Practica 3/Searcher/src/src/PanelOutput.java
@@ -0,0 +1,28 @@
+package src;
+
+import javax.swing.JPanel;
+import javax.swing.border.TitledBorder;
+import java.awt.Color;
+import javax.swing.JTextArea;
+
+public class PanelOutput extends JPanel {
+	private JTextArea textArea;
+
+	/**
+	 * Create the panel.
+	 */
+	public PanelOutput() {
+		setBorder(new TitledBorder(null, "Resultados", TitledBorder.LEADING, TitledBorder.TOP, null, Color.BLUE));
+		
+		textArea = new JTextArea();
+		textArea.setEditable(false);
+		textArea.setRows(10);
+		textArea.setColumns(40);
+		add(textArea);
+
+	}
+
+	public JTextArea getTextArea() {
+		return textArea;
+	}
+}
